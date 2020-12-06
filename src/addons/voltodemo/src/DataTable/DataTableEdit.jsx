026ca784457a4d99d98d4d3900bf98abc9abcdf1
David Ichim <ichim.david@gmail.com>
2020-12-06 14:57:44 +0200

    Chapter 5 final changes

--- src/addons/voltodemo/src/DataTable/DataTableEdit.jsx
+++ src/addons/voltodemo/src/DataTable/DataTableEdit.jsx
@@ -10,65 +10,36 @@ import tableSVG from '@plone/volto/icons/table.svg';
 import { TableSchema } from './schema';
 
 import DataTableView from './DataTableView';
+import { withBlockDataSource } from '@plone-collective/voltodemo/hocs';
 
 import './datatable-edit.less';
 
 const DataTableEdit = (props) => {
   const { selected, onChangeBlock, block, data } = props;
-  console.log(props);
   const schema = TableSchema(props);
 
   return (
-    <div className="dataTable-edit">
+    <>
       <SidebarPortal selected={selected}>
-        {!data.file_path?.length ? (
-          <Segment.Group raised>
-            <header className="header pulled">
-              <h2>Data table</h2>
-            </header>
-            <Segment className="sidebar-metadata-container" secondary>
-              No file selected
-              <Icon name={tableSVG} size="100px" color="#b8c6c8" />
-            </Segment>
-          </Segment.Group>
-        ) : (
-          <InlineForm
-            schema={schema}
-            title={schema.title}
-            onChangeField={(id, value) => {
-              onChangeBlock(block, {
-                ...data,
-                [id]: value,
-              });
-            }}
-            formData={data}
-          />
-        )}
+        <InlineForm
+          schema={schema}
+          title={schema.title}
+          onChangeField={(id, value) => {
+            onChangeBlock(block, {
+              ...data,
+              [id]: value,
+            });
+          }}
+          formData={data}
+        />
       </SidebarPortal>
-
-      {data.file_path?.length ? (
-        <DataTableView {...props} />
-      ) : (
-        <div className="no-value">
-          <Form>
-            <Icon name={tableSVG} size="100px" color="#b8c6c8" />
-            <Field
-              id="file_path"
-              widget="pick_object"
-              title="Pick a file"
-              value={data.file_path || []}
-              onChange={(id, value) => {
-                onChangeBlock(block, {
-                  ...data,
-                  [id]: value,
-                });
-              }}
-            />
-          </Form>
-        </div>
-      )}
-    </div>
+      <DataTableView {...props} />
+    </>
   );
 };
 
-export default DataTableEdit;
+export default withBlockDataSource({
+  icon: tableSVG,
+  title: 'Data table',
+  getFilePath: ({ data: { file_path } }) => file_path,
+})(DataTableEdit);
--- src/addons/voltodemo/src/hocs.jsx
+++ src/addons/voltodemo/src/hocs.jsx
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getRawContent } from '@plone-collective/voltodemo/actions';
 import csv from 'papaparse';
 
+import { Segment, Form } from 'semantic-ui-react';
+import { SidebarPortal, Field, Icon } from '@plone/volto/components';
+
 export const withFileData = (getFilePath) => (WrappedComponent) => {
   return (props) => {
     const file_path = getFilePath(props);
@@ -30,5 +33,54 @@ export const withFileData = (getFilePath) => (WrappedComponent) => {
     }, [content]);
 
     return <WrappedComponent {...props} file_data={file_data} />;
+  };
+};
+
+
+export const withBlockDataSource = (opts) => (WrappedComponent) => {
+  const { icon, title, getFilePath } = opts;
+
+  return (props) => {
+    const { data, selected, onChangeBlock, block } = props;
+    const file_path = getFilePath(props);
+
+    return (
+      <div className={`${data['@type']}-edit`}>
+        {!file_path ? (
+          <>
+            <div className="no-value">
+              <Form>
+                <Icon name={icon} size="100px" color="#b8c6c8" />
+                <Field
+                  id="file_path"
+                  widget="pick_object"
+                  title="Pick a file"
+                  value={file_path || []}
+                  onChange={(id, value) => {
+                    onChangeBlock(block, {
+                      ...data,
+                      [id]: value,
+                    });
+                  }}
+                />
+              </Form>
+            </div>
+
+            <SidebarPortal selected={selected}>
+              <Segment.Group raised>
+                <header className="header pulled">
+                  <h2>{title}</h2>
+                </header>
+                <Segment className="sidebar-metadata-container" secondary>
+                  No file selected
+                  <Icon name={icon} size="100px" color="#b8c6c8" />
+                </Segment>
+              </Segment.Group>
+            </SidebarPortal>
+          </>
+        ) : (
+          <WrappedComponent {...props} />
+        )}
+      </div>);
   };
 };
